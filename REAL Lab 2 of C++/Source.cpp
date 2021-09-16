caa447b7672badd8a6fb0d9924f3e807fa32d00c
Ty-S <schluter2@tritons.iowacentral.edu>
2021-09-15 22:21:58 -0500

    Add project files.

---
+++ REAL Lab 2 of C++/Source.cpp
@@ -0,0 +1,75 @@
+/*Ty Schluter
+*  C++ 1st Semester 2021
+*  15th of September
+*  Lab 2 and  Exploring Output
+* This lab is designed to get you familiar output to the command prompt using C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const string m = "Monday"; // Day Variables
+	const string t = "Tuesday";
+	const string w = "Wednesday";
+	const string th = "Thursday";
+	const string f = "Friday";
+	const string fct = "8:20";// Time Variables
+	const string sct = "10:00";
+	const string tct = "11:33";
+	const string foct = "12:57";
+	const string fict = "1:46";
+	const string sict = "2:35";
+	const string fc1 = "Macroeconomics 104";// Class Variables
+	const string fc2 = "Engineering Problems 102";
+	const string sc = "C++ 106";
+	const string tc = "Spanish 3 03";
+	const string foc = "Algebra 2 24";
+	const string fic1 = "Study Hall 05";
+	const string fic2 = "Physical Education 37";
+	const string sic = "American Literacy 14";
+	const string ck1 = "   *   *   *   *";//Checkerboard Variables
+	const string ck2 = " *   *   *   *";
+	cout << m << "\t\t" << fc1 << "\t\t" << fct << endl;
+	cout << m << "\t\t" << sc << "\t\t\t\t" << sct << endl;
+	cout << m << "\t\t" << tc << "\t\t\t" << tct << endl;
+	cout << m << "\t\t" << foc << "\t\t\t" << foct << endl;
+	cout << m << "\t\t" << fic1 << "\t\t\t" << fict << endl;
+	cout << m << "\t\t" << sic << "\t\t" << sict << endl;
+	cout << t << "\t\t" << fc2 << "\t" << fct << endl;
+	cout << t << "\t\t" << sc << "\t\t\t\t" << sct << endl;
+	cout << t << "\t\t" << tc << "\t\t\t" << tct << endl;
+	cout << t << "\t\t" << foc << "\t\t\t" << foct << endl;
+	cout << t << "\t\t" << fic2 << "\t\t" << fict << endl;
+	cout << t << "\t\t" << sic << "\t\t" << sict << endl;
+	cout << w << "\t" << fc1 << "\t" << fct << endl;
+	cout << w << "\t" << sc << "\t\t\t\t" << sct << endl;
+	cout << w << "\t" << tc << "\t\t\t" << tct << endl;
+	cout << w << "\t" << foc << "\t\t\t" << foct << endl;
+	cout << w << "\t" << fic1 << "\t\t\t" << fict << endl;
+	cout << w << "\t" << sic << "\t\t" << sict << endl;
+	cout << th << "\t" << fc2 << "\t" << fct << endl;
+	cout << th << "\t" << sc << "\t\t\t\t" << sct << endl;
+	cout << th << "\t" << tc << "\t\t\t" << tct << endl;
+	cout << th << "\t" << foc << "\t\t\t" << foct << endl;
+	cout << th << "\t" << fic2 << "\t\t" << fict << endl;
+	cout << th << "\t" << sic << "\t\t" << sict << endl;
+	cout << f << "\t\t" << fc1 << "\t\t" << fct << endl;
+	cout << f << "\t\t" << sc << "\t\t\t\t" << sct << endl;
+	cout << f << "\t\t" << tc << "\t\t\t" << tct << endl;
+	cout << f << "\t\t" << foc << "\t\t\t" << foct << endl;
+	cout << f << "\t\t" << fic1 << "\t\t\t" << fict << endl;
+	cout << f << "\t\t" << sic << "\t\t" << sict << endl;
+	cout << "\n\n\n" << endl;
+	cout << ck1 << endl;
+	cout << ck2 << endl;
+	cout << ck1 << endl;
+	cout << ck2 << endl;
+	cout << ck1 << endl;
+	cout << ck2 << endl;
+	cout << ck1 << endl;
+	cout << ck2 << endl;
+	return 0;
+}
